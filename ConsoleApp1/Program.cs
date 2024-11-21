e60c05de0b96bd376161fcc13bb6dca3112d8837
xStarRier <stephanniequynh@gmail.com>
2024-11-21 20:51:57 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+void printNoReturn(string message)
+
+{  
+   Console.WriteLine(message);
+   
+}
+printNoReturn("This Method Returns Nothing!");
+
